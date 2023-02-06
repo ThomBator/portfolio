e3897e0fb6721501ac16e40cdbe5deafdddf7d43
thombator <thom.bator@gmail.com>
2023-02-06 13:10:35 -0500

    updated config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,8 @@ import { astroImageTools } from 'astro-imagetools';
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
-  site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
-  // Important!
-  // Only official '@astrojs/*' integrations are currently supported by Astro.
-  // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
-  // with 'astro build' command.
+  site: 'https://thombator.github.io',
+  base: '/portfolio'
   experimental: {
     integrations: true,
   },
